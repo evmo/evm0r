fff281d47bc358d3ac0e44c4280aec731f9ab45c
Evan Morrison <emorrison@gmail.com>
2020-07-22 22:29:41 -0700

    randStr()

--- R/strings.R
+++ R/strings.R
@@ -24,3 +24,19 @@ urlify <- function(string) {
 asciify <- function(string) {
   stringi::stri_trans_general(string, "latin-ascii")
 }
+
+#' Generate random string
+#'
+#' @param n number of characters
+#'
+#' @return
+#' @export
+#'
+#' @examples
+randStr <- function(n) {
+  str <- vector(mode = 'character', length = n)
+  pool <- c(letters, LETTERS, c(0:9))
+  for (i in 1:n)
+    str[i] <- sample(pool, 1)
+  paste(str, collapse = '')
+}
